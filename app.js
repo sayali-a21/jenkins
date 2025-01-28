5fe5bf2d955d1c6eec816cec75757ffdc6a3e8ba
sayali-a21 <adhavsayali37@gmail.com>
2025-01-28 16:39:09 +0000

    hi sayali

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('hiii!');
+  res.send('hiiisayali!');
 });
  
 // Define the port number and start the server
